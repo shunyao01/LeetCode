d88433d55f2418e560b65141a7a38039a380691a
shunyao01 <shunyao0122@gmail.com>
2025-04-30 19:14:39 +0800

    Solve search a 2d matrix

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ A structured way to tackle coding interview problems by topic. Track your practi
 
 | Topic               | # Solved | Confidence Level |
 |---------------------|---------:|-----------------:|
-| arrays              |        9 |                3 |
+| arrays              |        10 |                3 |
 | backtracking        |        1 |                1 |
 | dynamic_programming |        1 |                1 |
 | graphs              |        4 |                1 |
---
+++ arrays/search_a_2d_matrix.py
@@ -0,0 +1,39 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        Perform search on 2d matrix and return index of the target
+
+        Args:
+            matrix: 2d matrix of integers
+            target: number to be searched
+
+        Return:
+            bool: if target is found
+
+        Time Complexity: O(log mn)
+        Space Complexity: O(1)
+        """
+        if not matrix: return False
+        
+        total = len(matrix) * len(matrix[0])
+        low = 0
+        high = total - 1
+        
+        while low <= high:
+
+            # get mid index, convert index to 2d
+            mid = (low + high) // 2
+            i = mid // len(matrix[0]) 
+            j = mid % len(matrix[0]) 
+
+            print(total, i, j, mid)
+
+            if matrix[i][j] > target:
+                high = mid - 1
+            elif matrix[i][j] < target:
+                low = mid + 1
+            else: # found
+                return True
+
+        return False
+        
